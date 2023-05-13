0c62a0ce6eac7b832bdcabd7afbdc2df73515ab9
Suleyman YALCINKAYA <yalcinkaya3434@gmail.com>
2023-05-13 15:03:21 +0300

    sum

--- main/IP5306.h
+++ main/IP5306.h
@@ -62,44 +62,44 @@
 
 
 /*SHUTDOWN_TIME_VALUE*/
-#define SHUTDOWN_8s                                 0
-#define SHUTDOWN_32s                                1
-#define SHUTDOWN_16s                                2
-#define SHUTDOWN_64s                                3
+#define SHUTDOWN_8s   0
+#define SHUTDOWN_32s  1
+#define SHUTDOWN_16s  2
+#define SHUTDOWN_64s  3
 
 /*CUT-OFF VOLTAGE RANGE*/
-#define CUT_OFF_VOLTAGE_0                                   0     // 4.14/4.26/4.305/4.35  V
-#define CUT_OFF_VOLTAGE_1                                   1     // 4.17/4.275/4.32/4.365 V
-#define CUT_OFF_VOLTAGE_2                                   2     // 4.185/4.29/4.335/4.38 V
-#define CUT_OFF_VOLTAGE_3                                   3     // 4.2/4.305/4.35/4.395  V
+#define CUT_OFF_VOLTAGE_0   0     // 4.14/4.26/4.305/4.35  V
+#define CUT_OFF_VOLTAGE_1   1     // 4.17/4.275/4.32/4.365 V
+#define CUT_OFF_VOLTAGE_2   2     // 4.185/4.29/4.335/4.38 V
+#define CUT_OFF_VOLTAGE_3   3     // 4.2/4.305/4.35/4.395  V
 
 /*BATTERY STOP CHARGING CURRENT*/
-#define CURRENT_200                                 0
-#define CURRENT_400                                 1
-#define CURRENT_500                                 2
-#define CURRENT_600                                 3
+#define CURRENT_200   0
+#define CURRENT_400   1
+#define CURRENT_500   2
+#define CURRENT_600   3
 
 /*VOLTAGE SETTING FOR CHARGING*/
-#define VOUT_0                                0   //4.45
-#define VOUT_1                                1   //4.5
-#define VOUT_2                                2   //4.55
-#define VOUT_3                                3   //4.6
-#define VOUT_4                                4   //4.65
-#define VOUT_5                                5   //4.7
-#define VOUT_6                                6   //4.75
-#define VOUT_7                                7   //4.8
+#define VOUT_0  0   //4.45
+#define VOUT_1  1   //4.5
+#define VOUT_2  2   //4.55
+#define VOUT_3  3   //4.6
+#define VOUT_4  4   //4.65
+#define VOUT_5  5   //4.7
+#define VOUT_6  6   //4.75
+#define VOUT_7  7   //4.8
 
 /*Battery Voltage */
-#define BATT_VOLTAGE_3                        3  //4.4
-#define BATT_VOLTAGE_2                        2  //4.35
-#define BATT_VOLTAGE_1                        1  //4.3
-#define BATT_VOLTAGE_0                        0  //4.2
+#define BATT_VOLTAGE_3  3  //4.4
+#define BATT_VOLTAGE_2  2  //4.35
+#define BATT_VOLTAGE_1  1  //4.3
+#define BATT_VOLTAGE_0  0  //4.2
 
 /*Voltage Pressure setting*/
-#define Pressurized_42                          3
-#define Pressurized_28                          2
-#define Pressurized_14                          1
-#define Not_pressurized                         0
+#define Pressurized_42   3
+#define Pressurized_28   2
+#define Pressurized_14   1
+#define Not_pressurized  0
 
 
 
